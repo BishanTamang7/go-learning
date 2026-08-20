4811084a72813eb30c461afae48a8c8d829775ee
Bishan Tamang <realbishantamang@gmail.com>
2026-08-20 18:12:27 +0545

    declare variable and assign a value and print variables

--- 01-basics/variables/main.go
+++ 01-basics/variables/main.go
@@ -1,1 +1,17 @@
-package variables
+package main
+
+import "fmt"
+
+func main() {
+	// Declare a variable of type int
+	var age int
+
+	// Assign a value to the variable
+	age = 22
+
+	var name string = "Bishan Tamang"
+
+	// Print the value of the variable
+	fmt.Println(age)
+	fmt.Println(name)
+}
