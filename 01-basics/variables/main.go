d9a54071cce4f090d0a800ceba134ca462aa8e71
Bishan Tamang <realbishantamang@gmail.com>
2026-08-21 12:11:50 +0545

    Declaring a variable and assign a value to the variable

--- 01-basics/variables/main.go
+++ 01-basics/variables/main.go
@@ -3,25 +3,60 @@ package main
 import "fmt"
 
 func main() {
+	/*
+		// Declaring a variable
+		var age int
+		// var = keyword to declare a variable
+		// age = variable name
+		// int = data type of the variable
+
+		// At this point, the variable 'age' has been declared but not initialized, so it holds the default value for its type, which is 0 for integers.
+		fmt.Println(age)
+		// Output: 0
+
+		// Practice
+		var name string
+		var isStudent bool
+		var height float64
+		var age1 int
+
+		fmt.Println(name)      // Output: (empty string)
+		fmt.Println(isStudent) // Output: false
+		fmt.Println(height)    // Output: 0
+		fmt.Println(age1)      // Output: 0
+	*/
+
+	// Declaring and initializing a variable
 
 	// Declaring a variable
 	var age int
-	// var = keyword to declare a variable
-	// age = variable name
-	// int = data type of the variable
 
-	// At this point, the variable 'age' has been declared but not initialized, so it holds the default value for its type, which is 0 for integers.
-	fmt.Println(age)
-	// Output: 0
+	// Assigning a value to the variable
+	age = 25
+
+	// Remember:
+	// Declaring -> Creating a variable and specifying its type.
+	// Assigning -> Giving a value to the variable.
+
+	fmt.Println(age) // Output: 25
 
 	// Practice
+	// Declaring variables
 	var name string
 	var isStudent bool
 	var height float64
 	var age1 int
 
-	fmt.Println(name)      // Output: (empty string)
-	fmt.Println(isStudent) // Output: false
-	fmt.Println(height)    // Output: 0
-	fmt.Println(age1)      // Output: 0
+	// Assigning values to the variables
+	name = "Bishan Tamang"
+	isStudent = true
+	height = 5.7
+	age1 = 22
+
+	// Printing the values of the variables
+	fmt.Println(name)      // Output: Bishan Tamang
+	fmt.Println(isStudent) // Output: true
+	fmt.Println(height)    // Output: 5.7
+	fmt.Println(age1)      // Output: 22
+
 }
