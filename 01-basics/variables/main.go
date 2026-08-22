546e56caa2df1f78484f409de29a57c24b6e6a5c
Bishan Tamang <realbishantamang@gmail.com>
2026-08-22 21:08:48 +0545

    Learning short variable declaration using :=

--- 01-basics/variables/main.go
+++ 01-basics/variables/main.go
@@ -79,20 +79,42 @@ func main() {
 		fmt.Println(age1)      // Output: 22
 	*/
 
-	// Type inference
-	// Go can infer the type of a variable based on the value assigned to it.
-	// This means you can declare and initialize a variable without explicitly specifying its type.
+	/*
+		// Type inference
+		// Go can infer the type of a variable based on the value assigned to it.
+		// This means you can declare and initialize a variable without explicitly specifying its type.
 
-	// Declaring and initializing a variable with type inference
-	var age = 25 // Go infers that 'age' is of type int
+		// Declaring and initializing a variable with type inference
+		var age = 25 // Go infers that 'age' is of type int
+
+		fmt.Println(age) // Output: 25
+
+		// Practice
+		var name = "Bishan Tamang"
+		var isStudent = true
+		var height = 5.7
+		var age1 = 22
+
+		fmt.Println(name)      // Output: Bishan Tamang
+		fmt.Println(isStudent) // Output: true
+		fmt.Println(height)    // Output: 5.7
+		fmt.Println(age1)      // Output: 22
+	*/
+
+	// Short variable declaration
+	// Go provides a shorthand syntax for declaring and initializing variables using the := operator.
+	// This is known as short variable declaration.
+
+	// Declaring and initializing a variable using short variable declaration
+	age := 25 // Go infers that 'age' is of type int
 
 	fmt.Println(age) // Output: 25
 
-	// Practice
-	var name = "Bishan Tamang"
-	var isStudent = true
-	var height = 5.7
-	var age1 = 22
+	// practice
+	name := "Bishan Tamang"
+	isStudent := true
+	height := 5.7
+	age1 := 22
 
 	fmt.Println(name)      // Output: Bishan Tamang
 	fmt.Println(isStudent) // Output: true
