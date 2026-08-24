cdd0c02873f130dad9c9fbec4fd594c904f140d3
Bishan Tamang <realbishantamang@gmail.com>
2026-08-24 20:57:11 +0545

    Learning basic data types

---
+++ 01-basics/data-types/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// In Go, data types are used to define the type of a variable. Each variable must have a specific data type, which determines the kind of values it can hold and the operations that can be performed on it.
+	// Basic data types in Go include:
+	// 1. int: Represents integer values.
+	var age int = 22
+	fmt.Println(age)
+
+	// 2. float64: Represents floating-point numbers.
+	var height float64 = 5.9
+	fmt.Println(height)
+
+	// 3. string: Represents a sequence of characters.
+	var name string = "Bishan Tamang"
+	fmt.Println(name)
+
+	// 4. bool: Represents boolean values (true or false).
+	var isStudent bool = true
+	fmt.Println(isStudent)
+
+}
