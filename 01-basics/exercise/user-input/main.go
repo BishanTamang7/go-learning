c2ad648ac93004b60d28ff284362a3e90cd8b191
Bishan Tamang <realbishantamang@gmail.com>
2026-08-28 15:12:13 +0545

    Complete Go user-input remaning exercise

--- 01-basics/exercise/user-input/main.go
+++ 01-basics/exercise/user-input/main.go
@@ -94,4 +94,75 @@ func main() {
 	fmt.Scan(&l)
 
 	fmt.Println(k % l)
+
+	// 6. Ask the user for their name1 and age1, then print:
+	// Hello Bishan, you are 22 years old.
+
+	var name1 string
+	var age1 int
+
+	fmt.Print("Enter Your Name1: ")
+	fmt.Scan(&name1)
+	fmt.Print("Enter Your Age1: ")
+	fmt.Scan(&age1)
+
+	fmt.Println("Hello,", name1, "you are", age1, "years old.")
+
+	// 7. Ask the user for the length and width of a rectangle. Calculate the area.
+	var length int
+	var width int
+
+	fmt.Print("Enter length of rectangle: ")
+	fmt.Scan(&length)
+	fmt.Print("Enter Width of rectangle: ")
+	fmt.Scan(&width)
+
+	fmt.Println(length * width)
+
+	// 8. Ask the user for the price and quantity of a product. Calculate the total price.
+	var price int
+	var quantity int
+
+	fmt.Print("Enter a price of a product: ")
+	fmt.Scan(&price)
+	fmt.Print("Enter a quantity of a product: ")
+	fmt.Scan(&quantity)
+
+	fmt.Println(price * quantity)
+
+	// 9. Ask the user for three numbers and calculate their average.
+	var num22 int
+	var num33 int
+	var num44 int
+
+	fmt.Print("Ente a Num22: ")
+	fmt.Scan(&num22)
+	fmt.Print("Enter a Num33: ")
+	fmt.Scan(&num33)
+	fmt.Print("Enter a Num44: ")
+	fmt.Scan(&num44)
+
+	fmt.Println((num22 + num33 + num44) / 2)
+
+	// 10. Challenge: Ask the user for:
+	// . Name7
+	// . Age7
+	// . Height7
+	// . Weight7
+	// Then print all information in a formatted way.
+	var name7 string
+	var age7 int
+	var height7 float64
+	var weight7 float64
+
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name7)
+	fmt.Print("Enter your age: ")
+	fmt.Scan(&age7)
+	fmt.Print("Enter your height: ")
+	fmt.Scan(&height7)
+	fmt.Print("Enter your weight: ")
+	fmt.Scan(&weight7)
+
+	fmt.Println("Hello, My name is", name7, ". I am", age7, "years old.", ".And i am ", height7, "feet", "And my Weight is", weight7)
 }
